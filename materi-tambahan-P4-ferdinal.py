0a78be4adb1639873d8492a779ef356ee72a44e7
ferdinalaxe <ferdinalraihan@gmail.com>
2022-10-10 11:08:04 +0700

    hapus print

--- materi-tambahan-P4-ferdinal.py
+++ materi-tambahan-P4-ferdinal.py
@@ -7,10 +7,6 @@ tes_kehamilan = bool(int(input("Apakah Tes Kehamilan/HCG masih Positif? Jika Ya 
 mulut_rahim = bool(int(input("Apakah Mulut Rahim/Cervix masih dalam kondisi menutup? Jika Ya (Ketik 1) atau Tidak (Ketik 0) : ")))
 nyeri_perut = bool(int(input("Apakah anda merasa nyeri di perut bagian bawah? Jika Ya (Ketik 1) atau Tidak (Ketik 0) : ")))
 
-print(telat_haid, usia_kehamilan, pendarahan, hasil_konsepsi, mulas, tes_kehamilan, mulut_rahim, nyeri_perut)
-
-
-
 if (telat_haid < 20) & (usia_kehamilan < 20) & (pendarahan == True) & (hasil_konsepsi == True) & (mulas == True) & (tes_kehamilan == True) & (mulut_rahim == True) & (nyeri_perut == True):
     print("Anda di diagnosa mengalami sakit Abortus Imminens")
 else:
