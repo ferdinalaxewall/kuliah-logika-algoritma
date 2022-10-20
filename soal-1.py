c9f346ab47a0eb959ec5f00d5263f412cf10d629
Ferdinalaxewall <86649049+ferdinalaxewall@users.noreply.github.com>
2022-10-20 19:47:01 +0700

    add

---
+++ soal-1.py
@@ -0,0 +1,8 @@
+nama = [['Ibu', 'Bapak', 'Neng', 'Abang', 'Paman', 'Bibi'], ['Sugeng', 'Ita', 'Memed', 'Djoko', 'Imut', 'Yoyoh', 'Blacky', 'Udin']]
+
+print(nama[0][1], nama[1][3])
+print(nama[0][0], nama[1][5])
+print(nama[0][-2], nama[1][0])
+print(nama[0][-1], nama[1][1])
+print(nama[0][-3], nama[1][2])
+print(nama[0][2], nama[1][4])
---
+++ soal-2.py
@@ -0,0 +1,7 @@
+my_tuple = (['Manila', 'Kairo', 'Tokyo', 'Jakarta', 'Bogor', 'Berlin', 'Sydney'], ['Jepang', 'Jerman', 'Mesir', 'Filiphina', 'Indonesia', 'Australia', 'Swiss'])
+
+print(f"Ibukota negara {my_tuple[1][0]} adalah {my_tuple[0][2]}")
+print(f"Ibukota negara {my_tuple[1][2]} adalah {my_tuple[0][1]}")
+print(f"Ibukota negara {my_tuple[1][-3]} adalah {my_tuple[0][3]}")
+print(f"Ibukota negara {my_tuple[1][-2]} adalah {my_tuple[0][-1]}")
+print(f"Ibukota negara {my_tuple[1][3]} adalah {my_tuple[0][0]}")
